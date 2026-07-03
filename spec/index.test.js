0b84d111219daddc0711203acf12c60f93a85b74
Maddy Guthridge <hello@maddyguthridge.com>
2026-07-03 18:10:22 +1000

    feat: support disabling default syntax

--- spec/index.test.js
+++ spec/index.test.js
@@ -16,6 +16,32 @@ describe('marked-extension-template', () => {
     t.assert.notStrictEqual(marked.parse(diagram), unmarked.parse(diagram));
   });
 
+  test('mermaid code block renders to HTML, ready to run mermaid', (t) => {
+    const diagram = [
+      '```mermaid',
+      'sequenceDiagram',
+      '  User->>Minifolio: Request document containing mermaid',
+      '  Minifolio-->>User: Uses Marked and MermaidJS to render the document',
+      '```',
+    ].join('\n');
+    const marked = new Marked(markedMermaid());
+    t.assert.strictEqual(marked.parse(diagram).includes('<pre class="mermaid">'), true);
+    t.assert.strictEqual(marked.parse(diagram).includes('</pre>'), true);
+  });
+
+  test('HTML mermaid diagrams are allowed', (t) => {
+    const diagram = [
+      '<pre class="mermaid">',
+      'sequenceDiagram',
+      '  User->>Minifolio: Request document containing mermaid',
+      '  Minifolio-->>User: Uses Marked and MermaidJS to render the document',
+      '</pre>',
+    ].join('\n');
+    const marked = new Marked(markedMermaid());
+    t.assert.strictEqual(marked.parse(diagram).includes('<pre class="mermaid">'), true);
+    t.assert.strictEqual(marked.parse(diagram).includes('</pre>'), true);
+  });
+
   test('Other code blocks are unaffected', (t) => {
     const js = [
       '```js',
